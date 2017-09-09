ef3913e80c3eff77752bf95945d5c4f98e7d6e3b
Istvan Kozma <pisti_kozma@hotmail.com>
2017-09-09 22:30:31 +0200

    feat(getComics.py) make downloaded image retain original extension

--- getComics.py
+++ getComics.py
@@ -2,6 +2,7 @@ from plyer import notification
 from bs4 import BeautifulSoup
 import requests
 
+
 result = requests.get("http://explosm.net/")
 html_doc = result.content
 soup = BeautifulSoup(html_doc, 'html.parser')
@@ -9,11 +10,13 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 latestComicAnchor = soup.find(href="/comics/latest/")
 latestComicUrl = "http:" + latestComicAnchor.img["src"]
 
-print(latestComicAnchor)
 
-''' img_data = requests.get(latestComicUrl).content
-with open('image_name.jpg', 'wb') as handler:
-    handler.write(img_data) '''
+
+img_data = requests.get(latestComicUrl)
+img_content = img_data.content
+img_extension = img_data.headers["content-type"].split("/")[1]
+with open('../comics/img.' + img_extension, 'wb') as handler:
+    handler.write(img_content) 
 
 
 
