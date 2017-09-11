716fa80529ff0da448f0219b4ecf469d1068a6c2
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 21:00:52 +0200

    refactor(getComics.py): remove obselete functions

--- getComics.py
+++ getComics.py
@@ -15,19 +15,6 @@ class ComicUrlParser:
         img_html_tag = self.parsed_html.find(id="main-comic")
         self.image_url = "http:" + img_html_tag["src"]
 
-''' def getLatestComic():
-    result = requests.get("http://explosm.net/comics/latest")
-    html_doc = result.content
-    soup = BeautifulSoup(html_doc, 'html.parser')
-
-def writeIdToConfig(img_id):
-    with open("config.txt", "w") as config:
-        config.write(img_id)
-
-def getImageUrl(soup):
-    img_tag = soup.find(id="main-comic")
-    return "http:" + img_tag["src"] '''
-
 
 def downloadImageData(latest_comic_url):
     img_data = requests.get(latest_comic_url)
