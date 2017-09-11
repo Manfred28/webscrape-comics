cb2d28bb04b487689c4ee7e599f9ad356c199a54
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 22:37:29 +0200

    feat(getComics.py): get url of last comic from RSS feed

--- getComics.py
+++ getComics.py
@@ -1,21 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
+import feedparser
 from imageDownloader import downloadImage
 
-class ComicUrlParser:
-    img_url = ""
-    
-    def parse_html(self, url):
-        result = requests.get(url)
+class ComicRssParser:
+
+    def __init__(self, rss):
+        self.rss = feedparser.parse(rss)
+        self.latest_comic_url= ""
+        self.image_url = ""
+
+    def get_latest_comic_url(self):
+        self.latest_comic_url = self.rss.entries[0].guid
+
+    def get_image_url(self):
+        result = requests.get(self.latest_comic_url)
         html_doc = result.content
         parsed_html = BeautifulSoup(html_doc, 'html.parser')
         img_html_tag = parsed_html.find(id="main-comic")
         self.image_url = "http:" + img_html_tag["src"]
 
-
 def main():
-    CAH_parser = ComicUrlParser()
-    CAH_parser.parse_html("http://explosm.net/comics/latest")
+    CAH_parser = ComicRssParser("https://explosm-1311.appspot.com/")
+    CAH_parser.get_latest_comic_url()
+    CAH_parser.get_image_url()
     downloadImage(CAH_parser.image_url, "./comics/CAH/")
 
 main()
