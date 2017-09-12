c5c60feb10b5166b0b2116df18eed23309e9419a
Kozma István <pisti_kozma@hotmail.com>
2017-09-12 21:01:57 +0200

    feat(imageDownloader.py): include comic id in downloaded img name

--- comicHandler.py
+++ comicHandler.py
@@ -5,11 +5,11 @@ from imageDownloader import downloadImage
 class ComicHandler:
     def getLatestCah(self):
         CAH = cah_parser()
-        downloadImage(CAH.image_download_url, "./comics/CAH/")
+        downloadImage(CAH.image_download_url, CAH.image_id, "./comics/CAH/")
 
     def getLatestXkcd(self):
         xkcd = xkcd_parser()
-        downloadImage(xkcd.image_url, "./comics/xkcd/")
+        downloadImage(xkcd.image_download_url, xkcd.image_id, "./comics/xkcd/")
 
 def main():
     handler = ComicHandler()
--- imageDownloader.py
+++ imageDownloader.py
@@ -1,11 +1,11 @@
 import requests
 import os
 
-def downloadImage(url, destination):
+def downloadImage(url, comic_id, destination):
     try: 
         img_data = requests.get(url)
         createDestinationFolder(destination)
-        createImageFile(img_data, destination)
+        createImageFile(img_data, comic_id, destination)
     except requests.exceptions.RequestException as e:
         print(e)
 
@@ -13,8 +13,8 @@ def createDestinationFolder(destination):
     if not os.path.exists(destination):
         os.makedirs(destination)
 
-def createImageFile(img_data, destination):
+def createImageFile(img_data, comic_id, destination):
     img_content = img_data.content
     img_extension = img_data.headers["content-type"].split("/")[1]
-    with open(destination + 'img.' + img_extension, 'wb') as handler:
+    with open(destination + 'img' + comic_id + "." + img_extension, 'wb') as handler:
         handler.write(img_content)
