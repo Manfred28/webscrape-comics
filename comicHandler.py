4717391e0233a7ff3d986238b68f3b7c14c77590
Kozma István <pisti_kozma@hotmail.com>
2017-09-13 10:13:22 +0200

    refactor(comicHandler): remove ComicHandler class

--- comicHandler.py
+++ comicHandler.py
@@ -2,18 +2,17 @@ from cahParser import cah_parser
 from xkcdParser import xkcd_parser
 from imageDownloader import download_image
 
-class ComicHandler:
-    def getLatestCah(self):
-        CAH = cah_parser()
-        download_image(CAH.comic_episodes, "./comics/CAH/")
 
-    def getLatestXkcd(self):
-        xkcd = xkcd_parser()
-        download_image(xkcd.comic_episodes, "./comics/xkcd/")
+def getLatestCah():
+    CAH = cah_parser()
+    download_image(CAH.comic_episodes, "./comics/CAH/")
+
+def getLatestXkcd():
+    xkcd = xkcd_parser()
+    download_image(xkcd.comic_episodes, "./comics/xkcd/")
 
 def main():
-    handler = ComicHandler()
-    handler.getLatestCah()
-    handler.getLatestXkcd()
+    getLatestXkcd()
+    getLatestCah()
 
 main()
