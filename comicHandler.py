d711bcea613027ddf413dde8d294f6b0fb3c980d
Kozma István <pisti_kozma@hotmail.com>
2017-09-13 07:46:08 +0200

    refactor(imageDownloader.py): rename function names to be consistent with styleguide

--- comicHandler.py
+++ comicHandler.py
@@ -1,15 +1,15 @@
 from cahParser import cah_parser
 from xkcdParser import xkcd_parser
-from imageDownloader import downloadImage
+from imageDownloader import download_image
 
 class ComicHandler:
     def getLatestCah(self):
         CAH = cah_parser()
-        downloadImage(CAH.image_download_url, CAH.image_id, "./comics/CAH/")
+        download_image(CAH.image_download_url, CAH.image_id, "./comics/CAH/")
 
     def getLatestXkcd(self):
         xkcd = xkcd_parser()
-        downloadImage(xkcd.image_download_url, xkcd.image_id, "./comics/xkcd/")
+        download_image(xkcd.image_download_url, xkcd.image_id, "./comics/xkcd/")
 
 def main():
     handler = ComicHandler()
--- imageDownloader.py
+++ imageDownloader.py
@@ -1,23 +1,23 @@
 import requests
 import os
 
-def downloadImage(url, comic_id, destination):
+def download_image(url, comic_id, destination):
     try: 
         img_data = requests.get(url)
-        createDestinationFolder(destination)
-        file_name = createFileName(img_data.headers, comic_id)
-        createImageFile(img_data.content, destination, file_name)
+        create_destination_folder(destination)
+        file_name = create_file_name(img_data.headers, comic_id)
+        create_image_file(img_data.content, destination, file_name)
     except requests.exceptions.RequestException as e:
         print(e)
 
-def createDestinationFolder(destination):
+def create_destination_folder(destination):
     if not os.path.exists(destination):
         os.makedirs(destination)
 
-def createFileName(img_data_headers, comic_id):
+def create_file_name(img_data_headers, comic_id):
     img_extension = img_data_headers["content-type"].split("/")[1]
     return "img" + comic_id + "." + img_extension
 
-def createImageFile(img_content, destination, file_name):
+def create_image_file(img_content, destination, file_name):
     with open(destination + file_name, 'wb') as handler:
         handler.write(img_content)
