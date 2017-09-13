fd7d566985f59180e112c2b61dba068cd6acf70c
Kozma István <pisti_kozma@hotmail.com>
2017-09-13 18:30:09 +0200

    feat(comicHandler.py): add notification when a new episode was downloaded

--- comicHandler.py
+++ comicHandler.py
@@ -1,3 +1,4 @@
+from plyer import notification
 from cahParser import cah_parser
 from xkcdParser import xkcd_parser
 from imageDownloader import download_image
@@ -5,11 +6,20 @@ from imageDownloader import download_image
 
 def getLatestCah():
     CAH = cah_parser()
-    download_image(CAH.comic_episodes, "./comics/CAH/")
+    number_of_episodes_downloaded = download_image(CAH.comic_episodes, "./comics/CAH/")
+    send_notification(number_of_episodes_downloaded, "Cyanide and Happiness")
 
 def getLatestXkcd():
     xkcd = xkcd_parser()
-    download_image(xkcd.comic_episodes, "./comics/xkcd/")
+    number_of_episodes_downloaded = download_image(xkcd.comic_episodes, "./comics/xkcd/")
+    send_notification(number_of_episodes_downloaded, "xkcd")
+
+def send_notification(episodes_downloaded, comic_name):
+    if episodes_downloaded > 0:
+        notification.notify(
+        title = comic_name,
+        message = str(episodes_downloaded) + " Episode(s) Downloaded"
+        )
 
 def main():
     getLatestXkcd()
--- imageDownloader.py
+++ imageDownloader.py
@@ -2,6 +2,7 @@ import os
 import requests
 
 def download_image(episodes, destination):
+    number_of_downloaded_episodes = 0
     for comic_id, url in episodes.items():
         try:
             img_data = requests.get(url)
@@ -10,8 +11,10 @@ def download_image(episodes, destination):
             file_name = create_file_name(img_data.headers, comic_id)
             if not does_file_exist(destination + file_name):
                 create_image_file(img_data.content, destination, file_name)
+                number_of_downloaded_episodes += 1
         except requests.exceptions.RequestException as e:
             print(e)
+    return number_of_downloaded_episodes
 
 def does_file_exist(path):
     return os.path.exists(path)
