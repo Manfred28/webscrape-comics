28a23f5b143b55a3f4907b78dd7ec55ae7f1177d
Kozma István <pisti_kozma@hotmail.com>
2017-09-14 09:56:01 +0200

    feat(comicHandler): filter already downloaded episodes before parsing

--- comicHandler.py
+++ comicHandler.py
@@ -6,16 +6,26 @@ from imageDownloader import download_image
 
 #Could be a simple function...
 class ComicHandler:
-    def __init__(self, comic, name, download_destination):
-        self.comic = comic()
+    def __init__(self, Comic, name, download_destination):
+        self.Comic = Comic()
         self.download_destination = download_destination
         self.name = name
 
         if not os.path.exists(self.download_destination):
             os.makedirs(self.download_destination)
-        number_of_episodes_downloaded = download_image(self.comic.comic_episodes, self.download_destination)
+        self.filter_already_downloaded_episodes()
+        self.Comic.get_episode_download_links()
+        number_of_episodes_downloaded = download_image(self.Comic.episode_download_links, self.download_destination)
         notifications.send_notification(number_of_episodes_downloaded, self.name)
 
+    def filter_already_downloaded_episodes(self):
+        new_episodes = {}
+        for comic_id in self.Comic.new_episodes.keys():
+            episode_file_name = "img" + comic_id
+            for downloaded_episodes in os.listdir(self.download_destination):
+                if not episode_file_name in downloaded_episodes:
+                    new_episodes[comic_id] = self.Comic.new_episodes[comic_id]
+        self.Comic.new_episodes = new_episodes
 
 def main():
     cah_handler = ComicHandler(cah_parser, "Cyanide and Happiness", "./comics/CAH/")
--- comicParser.py
+++ comicParser.py
@@ -10,21 +10,20 @@ class ComicRssHtmlParser:
         self.episode_id = ""
         self.parsed_html = ""
         self.episode_img_download_url = ""
-        self.comic_episodes = {}
-
-        self.get_comic_episodes()
+        self.new_episodes = {}
+        self.episode_download_links = {}
 
         for episode in self.rss.entries:
             self.comic_episode_url = episode.guid
             self.get_episode_id()
-            self.comic_episodes[self.episode_id] = self.comic_episode_url
+            self.new_episodes[self.episode_id] = self.comic_episode_url
 
-    def get_comic_episodes(self):
-        for episode_id in self.comic_episodes.keys():
-            self.comic_episode_url = self.comic_episodes[episode_id]
+    def get_episode_download_links(self):
+        for episode_id in self.new_episodes.keys():
+            self.comic_episode_url = self.new_episodes[episode_id]
             self.parse_comic_html()
             self.parse_episode_img_download_url()
-            self.comic_episodes[episode_id] = self.episode_img_download_url
+            self.episode_download_links[episode_id] = self.episode_img_download_url
 
     def get_episode_id(self):
         self.episode_id = self.comic_episode_url.split("/")[-2]
