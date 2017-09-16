ef29264866f44433b2942090298644094926d19c
Kozma István <pisti_kozma@hotmail.com>
2017-09-16 09:32:30 +0200

    refactor(imageDownloader): remove obselete code to filter downloaded files

--- downloader/imageDownloader.py
+++ downloader/imageDownloader.py
@@ -1,24 +1,16 @@
-import os
 import requests
 
 def download_image(episodes, destination):
     number_of_downloaded_episodes = 0
     for file_name, url in episodes.items():
-        if not already_downloaded(destination, file_name):
-            try:
-                img_data = requests.get(url)
-                file_name += "." + get_extension(img_data.headers)
-                create_image_file(img_data.content, destination, file_name)
-                number_of_downloaded_episodes += 1
-            except requests.exceptions.RequestException as e:
-                print(e)
+        try:
+            img_data = requests.get(url)
+            file_name += "." + get_extension(img_data.headers)
+            create_image_file(img_data.content, destination, file_name)
+            number_of_downloaded_episodes += 1
+        except requests.exceptions.RequestException as e:
+            print(e)
     return number_of_downloaded_episodes
-
-
-def already_downloaded(destination, file_name):
-    for name in os.listdir(destination):
-        if file_name in name:
-            return True
 
 
 def get_extension(img_data_headers):
